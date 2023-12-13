3963442dca3e29a7c6af068f09b74e98765e3f22
Nikita Semenov <narbert773@yandex.com>
2023-12-13 19:34:27 +0300

    styles to mainpage added

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,44 +1,48 @@
-// import React from 'react';
-
-// export default function NavBar() {
-//   return (
-//     <nav className="navbar navbar-expand bg-body-tertiary">
-//       <div className="container-fluid">
-//         <a className="navbar-brand" href="/">Зоопарк</a>
-//         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-//           <span className="navbar-toggler-icon" />
-//         </button>
-//         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-//           <div className="navbar-nav">
-//             <a className="nav-link active" aria-current="page" href="/animals">Животные</a>
-//             <a className="nav-link" href="/tarifs">Тарифы</a>
-//             <a className="nav-link"  href="/admin" style={{ marginLeft: 'auto' }}>Вход</a>
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
 import React from 'react';
 
 export default function NavBar() {
-    return (
-        <nav className="navbar navbar-expand bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">Zoo</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <a className="nav-link active" aria-current="page" href="/animals">Животные</a>
-                        <a className="nav-link" href="/tarifs">Тарифы</a>
-                    </div>
-                    <div className="navbar-nav ms-auto">
-                        <a className="nav-link" href="/admin" style={{ marginLeft: 'auto', marginRight: '40px' }}>Вход</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar navbar-expand" style={{ backgroundColor: '#e3f2fd' }}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/" style={{ fontSize: '25px' }}>
+          Zoo
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a
+              className="nav-link"
+              aria-current="page"
+              href="/animals"
+              style={{ marginLeft: '40px', fontSize: '20px' }}
+            >
+              Животные
+            </a>
+            <a className="nav-link" href="/tarifs" style={{ marginLeft: '40px', fontSize: '20px' }}>
+              Тарифы
+            </a>
+          </div>
+          <div className="navbar-nav ms-auto">
+            <a
+              className="nav-link"
+              href="/admin"
+              style={{ marginLeft: 'auto', marginRight: '40px', fontSize: '20px' }}
+            >
+              Вход
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
